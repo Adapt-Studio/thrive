67734b09cb757edb246465c2725553612764e2e6
Mac <65713232+machooper@users.noreply.github.com>
2020-11-22 13:05:45 +0000

    fix: completed base site

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0de151de48b14075ae6a";
+/******/ 	var hotCurrentHash = "17bb2809591efa4b1a25";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/header.js
+++ components/header.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 export default function () {
 	return (
-	<header className="w-screen h-24 flex justify-evenly items-end">
+	<header className="w-screen h-24 flex justify-evenly items-end font-head text-2xl">
 		<nav className="flex justify-evenly items-end space-x-24 -mr-24">
-			<a href="#home">Home</a>
-			<a href="#food">Food</a>
+			<a className="hover:text-green-200"  href="#home">Home</a>
+			<a className="hover:text-green-200"  href="#food">Food</a>
 		</nav>
 		<Image
 			src="/icons/logo.png" 
@@ -13,8 +13,8 @@ export default function () {
 			height={60}
 		/>
 		<nav className="flex justify-evenly items-end space-x-24 -ml-24">
-			<a href="#order">Order</a>
-			<a href="#contact">Contact</a>
+			<a className="hover:text-green-200" href="#order">Order</a>
+			<a className="hover:text-green-200" href="#contact">Contact</a>
 		</nav>
 	</header>
 	)
